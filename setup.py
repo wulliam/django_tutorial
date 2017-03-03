295c358d632639249f5c8e5d905ef24673c8519d
wuliang <wulliam@gmail.com>
2017-03-03 15:09:08 +0800

    upddate python version

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
